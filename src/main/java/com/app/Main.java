fff26d3059671cbf1aa5922a79eb0d63b5462496
Brenda Anievski <135037211+brendaanievskimts@users.noreply.github.com>
2026-04-15 21:30:03 +0000

    :sparkles: adding testing cases and descombulator class

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo! Este é o projeto Descombobulator com Java 21.");
+        Descombulator descombulator = new Descombulator();
+        descombulator.executar();
     }
 }
